c8f83dae12eddf295d24a98db72b084b2c39d5c4
Vinicius 'Davina' Ferreira <63323842+davinnye@users.noreply.github.com>
2024-04-08 20:46:40 -0300

    logo responsivo adicionado

--- src/components/topbanner/index.jsx
+++ src/components/topbanner/index.jsx
@@ -1,8 +1,9 @@
 import "./style.css";
-
+import Astarion from "./imgs/Astarion.png"
 export function Topbanner(){
     return(
         <div className="topbanner">
+            <img src={Astarion} alt="Astarion" className="topbanner"/>
             <center className="topbanner">GERENCIADOR DE FILMES</center>
         </div>
     )
