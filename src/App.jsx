36529b51c7e439f61dfb879ca08330447a1cade6
Vinicius 'Davina' Ferreira <63323842+davinnye@users.noreply.github.com>
2024-04-08 23:03:28 -0300

    adição da page home

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Menu } from "./components/menu";
 import { Topbanner } from "./components/topbanner";
 import { Topbar } from "./components/topbar";
 import "./global.css";
+import { Home } from "./pages/home";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Topbanner/>
         <Mainbody>
         <Menu/>
+        <Home/>
         </Mainbody>
         <Footer/>
         </>
